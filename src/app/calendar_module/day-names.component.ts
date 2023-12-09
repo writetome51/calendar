60537cdcdbec0193cddbf78650d717ccae71263d
Steve Thompson <writetome51@gmail.com>
2023-12-09 00:17:59 -0700

    Update DayNamesComponent:
    Allows you to change the day the week begins on

--- src/app/calendar_module/day-names.component.ts
+++ src/app/calendar_module/day-names.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 
 @Component({
    selector: 'day-names',
    template: `
       <week-block>
-         <div class="day-column" *ngFor="let dayName of data">
+         <div class="day-column" *ngFor="let dayName of days">
             <div class="calendar-day day-name">{{dayName}}</div>
          </div>
       </week-block>
@@ -16,8 +16,37 @@ import {Component} from '@angular/core';
       }`
    ]
 })
-export class DayNamesComponent {
+export class DayNamesComponent implements OnInit, OnChanges {
 
-   readonly data = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+   @Input() weekBeginsOn: number = 0;
+   days: string[] = [];
+
+   private readonly __data = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+   ngOnInit() {
+      this.days = this.__getDays();
+   }
+
+   ngOnChanges() {
+      this.days = this.__getDays();
+   }
+
+   private __getDays() {
+      const days: string[] = [];
+
+      for (
+         let i = this.weekBeginsOn, length = this.__data.length;
+         i < length;
+         ++i
+      ) {
+         days.push(this.__data[i]);
+      }
+
+      for (let i = 0; i < this.weekBeginsOn; ++i) {
+         days.push(this.__data[i]);
+      }
+
+      return days;
+   }
 
 }
